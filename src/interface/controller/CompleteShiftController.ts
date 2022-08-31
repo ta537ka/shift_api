a88dec04f5ca19ea0e45380178e51d75e9b3fd8f
ta537ka <ta64ka10@gmail.com>
2022-08-31 21:52:26 +0900

    create complete_shift until controller

---
+++ src/interface/controller/CompleteShiftController.ts
@@ -0,0 +1,65 @@
+import { CreateCompleteShift } from "../../usecase/complete_shifts/CreateCompleteShift";
+import { DeleteCompleteShift } from "../../usecase/complete_shifts/DeleteCompleteShift";
+import { GetCompleteShift } from "../../usecase/complete_shifts/GetCompleteShift";
+import { ListCompleteShiftsByAdmin } from "../../usecase/complete_shifts/ListCompleteShiftsByAdmin";
+import { ListCompleteShiftsByStaff } from "../../usecase/complete_shifts/ListCompleteShiftsByStaff";
+import { UpdateCompleteShift } from "../../usecase/complete_shifts/UpdateCompleteShift";
+import { CompleteShiftRepository } from "../database/CompleteShiftRepository";
+import { IDBConnection } from "../database/IDBConnection";
+import { CompleteShiftSerializer } from "../serializer/CompleteShiftSerializer";
+
+export class CompleteShiftController {
+    private completeShiftSerializer: CompleteShiftSerializer;
+    private completeShfitRepository: CompleteShiftRepository;
+
+    constructor(dbConnection: IDBConnection) {
+        this.completeShiftSerializer = new CompleteShiftSerializer();
+        this.completeShfitRepository = new CompleteShiftRepository(dbConnection);
+    }
+
+    //admin
+    async findAllByAdmin(req: any, res: any) {
+        const { start_date, end_date } = req.body;
+        const useCase = new ListCompleteShiftsByAdmin(this.completeShfitRepository);
+        const results = await useCase.execute(start_date, end_date);
+        return this.completeShiftSerializer.serialize(results);
+    }
+
+    async findCompleteShift(req: any, res: any) {
+        const id = req.parans.id;
+        const useCase = new GetCompleteShift(this.completeShfitRepository);
+        const result = await useCase.execute(id);
+        return this.completeShiftSerializer.serialize(result);
+    }
+
+    async createCompleteShift(req: any, res: any) {
+        const { staff_id, status_id, day } = req.body;
+        const useCase = new CreateCompleteShift(this.completeShfitRepository);
+        const result = await useCase.execute(staff_id, status_id, day);
+        return this.completeShiftSerializer.serialize(result);
+    }
+
+    async updateCompleteShit(req: any, res: any) {
+        const id = req.params.id;
+        const { status_id, day } = req.body;
+        const useCase = new UpdateCompleteShift(this.completeShfitRepository);
+        const result = await useCase.execute(id, status_id, day);
+        return this.completeShiftSerializer.serialize(result);
+    }
+
+    async deleteCompleteShift(req: any, res: any) {
+        const id = req.params.id;
+        const useCase = new DeleteCompleteShift(this.completeShfitRepository);
+        const result = await useCase.execute(id);
+        return this.completeShiftSerializer.serialize(result);
+    }
+
+
+    //staff
+    async findAllByStaff(req: any, res: any) {
+        const { start_date, end_date } = req.body;
+        const useCase = new ListCompleteShiftsByStaff(this.completeShfitRepository);
+        const results = await useCase.execute(start_date, end_date);
+        return this.completeShiftSerializer.serialize(results);
+    }
+}
---
+++ src/interface/database/CompleteShiftRepository.ts
@@ -0,0 +1,126 @@
+import { CompleteShift } from "../../domain/CompleteShift";
+import { ICompleteShiftRepository } from "../../usecase/repository/ICompleteShiftRepository";
+import { IDBConnection } from "./IDBConnection";
+
+export class CompleteShiftRepository extends ICompleteShiftRepository {
+    private connection: any;
+
+    constructor(connection: IDBConnection) {
+        super();
+        this.connection = connection;
+    }
+
+    private convertModel(r: any) {
+        const completeShift = new CompleteShift();
+        completeShift.id = r.id;
+        completeShift.staff_id = r.staff_id;
+        completeShift.status_id = r.status_id;
+        completeShift.day = r.day;
+        return completeShift;
+    }
+
+    //admin
+    async findAllByAdmin(start_date: string, end_date: string): Promise<CompleteShift[]> {
+        var query;
+        if (start_date == '' && end_date == '') {
+            query = await this.connection.execute(
+                "SELECT * FROM CompleteShifts"
+            );
+        } else if (start_date == '') {
+            const dt = new Date(end_date);
+            query = await this.connection.execute(
+                // `SELECT * FROM Shifts WHERE day <= STR_TO_DATE(${end_date}, '%Y-%m-%d')`
+                "SELECT * FROM CompleteShifts WHERE day <= ?",
+                [dt]
+            );
+        } else if (end_date == '') {
+            const dt = new Date(start_date);
+            query = await this.connection.execute(
+                "SELECT * FROM CompleteShifts WHERE ? <= day",
+                [dt]
+                // `SELECT * FROM Shifts WHERE STR_TO_DATE(${start_date}, '%Y-%m-%d') <= day`
+            );
+        } else {
+            const start_dt = new Date(start_date);
+            const end_dt = new Date(end_date);
+            query = await this.connection.execute(
+                "SELECT * FROM CompleteShifts WHERE day BETWEEN ? AND ?",
+                // [start_date, end_date]
+                [start_dt, end_dt]
+            );
+        }
+        const results = query.map((result: number | string | Date) => {
+            return this.convertModel(result);
+        });
+        return results;
+    }
+
+    async find(id: number): Promise<CompleteShift> {
+        const result = await this.connection.execute(
+            "SELECT * FROM CompleteShifts WHERE id = ?",
+            id
+        );
+        return result;
+    }
+
+    async persist(completeShift: CompleteShift): Promise<CompleteShift> {
+        const result = await this.connection.execute(
+            "INSERT INTO CompleteShifts(staff_id, status_id, day) VALUES(?,?,?)",
+            [completeShift.staff_id, completeShift.status_id, completeShift.day]
+        );
+        completeShift.id = result.id;
+        return completeShift;
+    }
+
+    async update(completeShift: CompleteShift): Promise<CompleteShift> {
+        const result = await this.connection.execute(
+            "UPDATE CompleteShifts SET status_id = ?, day = ? WHERE id = ?",
+            [completeShift.status_id, completeShift.day, completeShift.id]
+        );
+        return result;
+    }
+
+    async delete(id: number): Promise<CompleteShift> {
+        const result = await this.connection.execute(
+            "DELETE FROM CompleteShifts WHERE id = ?",
+            id
+        );
+        return result;
+    }
+
+    //staff
+    async findAllByStaff(start_date: string, end_date: string): Promise<CompleteShift[]> {
+        var query;
+        if (start_date == '' && end_date == '') {
+            query = await this.connection.execute(
+                "SELECT * FROM CompleteShifts"
+            );
+        } else if (start_date == '') {
+            const dt = new Date(end_date);
+            query = await this.connection.execute(
+                // `SELECT * FROM Shifts WHERE day <= STR_TO_DATE(${end_date}, '%Y-%m-%d')`
+                "SELECT * FROM CompleteShifts WHERE day <= ?",
+                [dt]
+            );
+        } else if (end_date == '') {
+            const dt = new Date(start_date);
+            query = await this.connection.execute(
+                "SELECT * FROM CompleteShifts WHERE ? <= day",
+                [dt]
+                // `SELECT * FROM Shifts WHERE STR_TO_DATE(${start_date}, '%Y-%m-%d') <= day`
+            );
+        } else {
+            const start_dt = new Date(start_date);
+            const end_dt = new Date(end_date);
+            query = await this.connection.execute(
+                "SELECT * FROM CompleteShifts WHERE day BETWEEN ? AND ?",
+                // [start_date, end_date]
+                [start_dt, end_dt]
+            );
+        }
+        const results = query.map((result: number | string | Date) => {
+            return this.convertModel(result);
+        });
+        return results;
+    }
+}
